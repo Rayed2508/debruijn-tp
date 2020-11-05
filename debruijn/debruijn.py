9d46944d0ceaf80b27c62473576e2803d9a7e918
Rayed2508 <rayedeidd@gmail.com>
2020-11-05 18:15:13 +0100

    Update debruijn.py

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -70,7 +70,7 @@ def read_fastq(fastq_file):
     with open(fastq_file, "r") as file:
         for line_number, n in enumerate(file):
             if line_number % 4 == 1:
-            yield n.strip()
+                yield n.strip()
 
 
 
